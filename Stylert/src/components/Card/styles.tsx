d22846b5a0d77f3862cadc1f59285dcf499ad4f2
William Mendonça <william@tecmasternet.com.br>
2020-12-19 06:44:46 -0300

    Iniciando Container

--- Stylert/src/components/Card/styles.tsx
+++ Stylert/src/components/Card/styles.tsx
@@ -1,8 +1,13 @@
-import styled from 'styled-components/native';
+import styled, { ThemeConsumer } from 'styled-components/native';
 
 export const Container = styled.View`
     height: 72px;
     padding: 14px;
     margin: 24px;
+    border-radius: 4px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: ${({theme}) => theme.colors['neutral-200']};
     
 `;
